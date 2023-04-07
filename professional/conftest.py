6fbbe874da82e84429349076be0fd44b0eca98c7
nggweb <nggwebdesign@gmail.com>
2023-04-07 22:19:23 +0000

    first commit

---
+++ professional/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from professional.users.models import User
+from professional.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
